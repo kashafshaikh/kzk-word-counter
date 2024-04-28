09a53f918aa9a86b56ffdf80235af12eca2209a6
kashafshaikh <160122626+kashafshaikh@users.noreply.github.com>
2024-04-29 03:48:18 +0500

    push my word counter project to github

---
+++ README.md
@@ -0,0 +1,1 @@
+"# kzk-word-counter" 
---
+++ index.js
@@ -0,0 +1,15 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+//Declare a constant 'answers' and assign it to the rsult of inquirer .prompt function
+const answers = await inquirer.prompt([
+    {
+        name: "Sentence",
+        type: "input",
+        message: "Enter a sentence to count the word: "
+    }
+]);
+const words = answers.Sentence.trim().split("");
+//Print the array of words to console
+console.log(words);
+//Print the word count of the sentence to the console
+console.log(`Your sentence word count is ${words.length}`);
